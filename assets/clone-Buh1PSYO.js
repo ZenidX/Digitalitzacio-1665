4c6ea2974e1861e62efbf7a6890bee9f840baa70
zenidx <zenid77@gmail.com>
2026-09-15 23:09:05 +0200

    Recursos descarregables: materials del modul, exemple resolt i enllac al video

---
+++ assets/clone-Buh1PSYO.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-DvUfH03_.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/clone-gH0Tx3zG.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-75Eyu_9E.js";var e=4;function a(o){return r(o,e)}export{a as c};
